1eff7a85fc29cc72f987eff1c30423cf79486931
Robert Davidson <rdavidson@accusoft.com>
2021-12-31 16:45:47 -0700

    Rethink how static pages will look
    Also add a favicon

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import express from "express";
 const app = express();
 const port = 3000;
+const cwd = process.cwd();
+
+app.use(express.static("files"));
+
+const staticPages = [
+    "index",
+    "thing",
+]
+for (const page of staticPages) {
+    app.get(`/${page}`, (req, res) => {
+        res.sendFile(`html/${page}.html`, {root: cwd});
+    });
+}
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.sendFile("html/index.html", {root: cwd});
 });
+
 
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
